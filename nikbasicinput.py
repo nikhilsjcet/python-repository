e837dfa7b6fdcff91c374687735a7d46010e0a95
nikhilsjcet <nikhilsaju2028@cs.sjcetpalai.ac.in>
2024-10-18 14:39:22 +0530

    Add files via upload

---
+++ nikbasicinput.py
@@ -0,0 +1,14 @@
+
+'''
+Author : Nikhil
+Date: 18-10-2024
+Description : program that asks the user to input their name and age.
+After receiving the input, display a message that includes the user's name and age.
+Hello, John! You are 25 years old.
+'''
+
+
+
+name = input("enter your name")
+age = input("enter your age")
+print("Hello,"+name+"! You are " + age+" years old")
