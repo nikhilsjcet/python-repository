a9e86a33177dcf5b73e9f96231ba7f20230e4d8a
nikhilsjcet <nikhilsaju2028@cs.sjcetpalai.ac.in>
2024-10-18 15:09:48 +0530

    Add files via upload

---
+++ nikstringdemo.py
@@ -0,0 +1,15 @@
+'''
+Author : Nikhil
+Date: 18-10-2024
+Python program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate it with another string.
+Finally, display the new string.
+'''
+
+
+
+
+str_var1=("Hello, World!")
+str_var2=str_var1[7:12]
+str_var3=" Everyone!"
+print(str_var2+str_var3)
