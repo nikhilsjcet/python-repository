08ab22e7e148d525ab31ba5ec01b88829bfce2d0
nikhilsjcet <nikhilsaju2028@cs.sjcetpalai.ac.in>
2024-10-19 15:50:36 +0530

    Add files via upload

--- Stringscat.py
+++ Stringscat.py
@@ -1,3 +1,27 @@
+'''
+Author: Nikhil Saju
+Date: 19-18-2024
+Description:program that performs the following tasks:
+    Create two separate strings:
+        The first string should contain your first name.
+        The second string should contain your last name.
+    Concatenate the two strings with a space in between
+    and store the result in a new variable.
+    Print the concatenated string.
+    From the concatenated string:
+        Access and print a sub-string that consists of the last name only.
+        Use string slicing to extract the sub-string.
+Enter your first name:NIkhil
+Enter your last name:Saju
+NIkhil Saju
+5
+ Saju
+ NIkhil
+
+
+
+'''
+
 first_name = input("Enter your first name")
 last_name =  input("Enter your last name")
 name= first_name+" " + last_name
