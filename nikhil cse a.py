121f7e6c53c92c361f52ebab6a5347e16ed68825
nikhilsjcet <nikhilsaju2028@cs.sjcetpalai.ac.in>
2024-10-19 16:28:21 +0530

    Add files via upload

---
+++ nikhil cse a.py
@@ -0,0 +1,41 @@
+'''print("hello,\n world")
+if 5 > 3:
+ print("five is greater than two")
+x = 5
+y = "thomas"
+print(x)
+print(y)
+x = "thomas"
+print(x)
+'''
+
+x = str(3) # x will be '3'
+y = int(3)
+z = float(3)
+print(type(x))
+print(type(y))
+print(type(z))
+x=y=z = "orange"
+
+print(x)
+print(y)
+print(z)
+x="Python "
+y="is "
+z="awesome "
+print(x +  y +   z)
+
+'''
+
+username = input("enter username")
+print("username is: "+ username )
+
+'''
+
+a="hello"
+b=" world"
+c= a + b
+print(c)
+
+x= " Hello world"
+print(x[2])
