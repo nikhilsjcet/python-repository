51dc2c8611bbe2dd5eb9a4216bb2f84fec704efd
nikhilsjcet <nikhilsaju2028@cs.sjcetpalai.ac.in>
2024-10-25 16:28:54 +0530

    Add files via upload

---
+++ posandneg.py
@@ -0,0 +1,7 @@
+number=int(input("Enter a number"))
+if number>0:
+    print(number,"is a positive number")
+elif number<0:
+    print(number,"is a negative number")
+else:
+    print(number,"is a zero")
