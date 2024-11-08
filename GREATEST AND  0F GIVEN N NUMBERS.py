2ce06a221be10113809c05bf9ce23b85b49bf17b
nikhilsjcet <nikhilsaju2028@cs.sjcetpalai.ac.in>
2024-11-08 15:25:18 +0530

    Update and rename GREATEST 0F GIVEN N NUMBERS.py to GREATEST AND  0F GIVEN N NUMBERS.py

--- GREATEST AND  0F GIVEN N NUMBERS.py
+++ GREATEST AND  0F GIVEN N NUMBERS.py
@@ -1,3 +1,10 @@
+'''
+Author : Nikhil saju
+Date:8/11/2024
+Description:Code to find the greatest and smallest of given n numbers
+'''
+
+
 n=int(input("Enter the list of numbers"))
 n1=int(input("Enter the first number:"))
 smallest=n1
